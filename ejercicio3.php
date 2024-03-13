bc9fa9c8821b85c51fbac1d3c191bbc261b6e237
Aleixito <aleixtomu@gmail.com>
2024-03-13 11:17:09 +0100

    3rd

--- ejercicio3.php
+++ ejercicio3.php
@@ -1,5 +1,24 @@
 
 <?php
 
+function esPrimero($numero) {
+    if ($numero <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($numero); $i++) {
+        if ($numero % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$sumaDePrimeros = array_reduce($numeros, function ($carry, $item) {
+    return esPrimero($item) ? $carry + $item : $carry;
+});
+
+echo $sumaDePrimeros; 
 
 ?>
