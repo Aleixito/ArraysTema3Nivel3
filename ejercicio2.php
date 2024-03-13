481b31ae1b39136e6128b9c17c794f29d42c25c8
Aleixito <aleixtomu@gmail.com>
2024-03-13 11:07:36 +0100

    2nd

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,5 +1,14 @@
 
 <?php
 
+$array = array("manzana", "banana", "tomate", "aguacate", "uva", "melocoton");
+
+function filtroCadenaLongitudPar($string) {
+    return strlen($string) % 2 == 0;
+}
+
+$arrayFiltrado = array_filter($array, "filtroCadenaLongitudPar");
+
+print_r($arrayFiltrado);
 
 ?>
